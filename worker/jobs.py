b578fd7af18edd9a9e27ae9378d6a655344a3d1e
mfloresVicomtech <46715684+mfloresVicomtech@users.noreply.github.com>
2023-07-14 11:54:19 +0000

    Rework tasks to accept variable amount of args.

--- worker/jobs.py
+++ worker/jobs.py
@@ -1,15 +1,20 @@
 import time
 from datetime import datetime as dt
 
-def do_long_task(delay:int=1):
-    # Do some long task here
+
+# Do a long task based on the specified delay:
+def long_task(args):
+    delay = int(args.get('delay', 1))
     print(f"Doing long task: delay {delay} sec")
     time.sleep(delay)
     return f"Some long task done in {delay} seconds"
 
-def do_long_io_task(amount:int=1000000000):
+
+# Do a IO intensive task based on a specified amount of data to read:
+def io_task(args):
     '''File operations (such as logging) can block the
     event loop: run them in a thread pool.'''
+    amount = int(args.get('amount', 1000000000))
     print(f'[{dt.now()}] {{blocking_io}}: Starting')
     with open('/dev/urandom', 'rb') as f:
         start_time = dt.now()
@@ -17,12 +22,16 @@ def do_long_io_task(amount:int=1000000000):
         print(f'[{dt.now()}] {{blocking_io}}: took {dt.now()-start_time}')
         return resp
 
-def do_long_cpu_task(amount:int=7):
+
+# Do a CPU intensive task based on a specified power to elevate 10 to for a math operation:
+def cpu_task(args):
     '''CPU-bound operations will block the event loop:
     in general it is preferable to run them in a
     process pool.'''
+    power = int(args.get('power', 7))
     print(f'[{dt.now()}] {{cpu_bound}}: Starting')
     start_time = dt.now()
-    resp = sum(i * i for i in range(10 ** amount))
+    resp = sum(i * i for i in range(10 ** power))
     print(f'[{dt.now()}] {{cpu_bound}}: took {dt.now()-start_time}')
     return resp
+
